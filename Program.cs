b58185fd4c29764ec024a7aa3764c8c1a65e3707
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-02 22:45:23 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace EmailValidateDemo
     {
         public static void Main(string[] args)
         {
-            string email = " abc.100@abc.com.au";
+            string email = " abc@1.com";
 
-            if (Regex.IsMatch(email, "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$"))
+            if (Regex.IsMatch(email, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$"))
             {
                 Console.WriteLine("Valid");
             }
