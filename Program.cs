df5d98e630f48e8713baedc018250432876c51f6
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-02 22:43:55 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -3,27 +3,19 @@
 namespace EmailValidateDemo
 {
     public class Program
+    {
+        public static void Main(string[] args)
         {
-            public static void Main(string[] args)
-            {
-                string[] emails = new string[] { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", ".abc@abc.com" };
-                foreach (string email in emails)
-                {
-                    if (IsValidEmail(email))
-                        Console.WriteLine(email + " is valid");
-                    else
-                        Console.WriteLine(email + " is invalid");
-                }
-            }
-            public static bool IsValidEmail(string email)
-            {
-                string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
-                    + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
-                    + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+            string email = "abc111@abc.com";
 
-                Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
-                return regex.IsMatch(email);
+            if (Regex.IsMatch(email, "^[a - zA - Z0 - 9._ % +-] +@[a - zA - Z0 - 9.-] +.[a-zA - Z]{ 2,4}$"))
+            {
+                Console.WriteLine("Valid");
             }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
     }
-    
 }
