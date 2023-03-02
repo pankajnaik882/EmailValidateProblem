c02cc4f2deebc62e53d0837b4472c40cd49a0ba5
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-02 22:39:58 +0530

    Add : Added Program.cs File

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmailValidateDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string email = "abc@yahoo.com";
+
+            if (Regex.IsMatch(email, @"^[a-zA-Z0-9@.]+$"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        } 
+    }
+}
