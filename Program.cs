d1ff879270bffc55f8cddbf453561f0103d41e9a
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-02 22:41:43 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -3,25 +3,27 @@
 namespace EmailValidateDemo
 {
     public class Program
-    {
+        {
+            public static void Main(string[] args)
+            {
+                string[] emails = new string[] { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", ".abc@abc.com" };
+                foreach (string email in emails)
+                {
+                    if (IsValidEmail(email))
+                        Console.WriteLine(email + " is valid");
+                    else
+                        Console.WriteLine(email + " is invalid");
+                }
+            }
+            public static bool IsValidEmail(string email)
+            {
+                string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                    + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                    + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
 
-        public static bool func()
-        {
-            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-            string email = "abc-100@yahoo.com";
-            Match match = regex.Match(email);
-            if (match.Success)
-            {
-                return true;
+                Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+                return regex.IsMatch(email);
             }
-            else
-            {
-                return false;
-            }
-        }
-        public static void Main(string[] args)
-        {
-            Program.func();
-        } 
     }
+    
 }
